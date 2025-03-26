1878f49deab12daacb3f449430be5603c91a1373
Patrick Casseus <patrickcjunior@gmail.com>
2025-03-26 14:29:50 -0400

    Fix import path and render Counter component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Counter from "./Counter.jsx";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -17,10 +17,8 @@ function App() {
         </a>
       </div>
       <h1>EcoLocation</h1>
+      <Counter />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
