f0df8290c6189d4e770300d6f4943c3aaad0a6d5
Patrick Casseus Jr. <pc59613n@pace.edu>
2025-04-02 15:57:46 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
 // App.jsx
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import LoginForm from './components/LoginForm';
-import About from './components/About';
-import Home from './components/Home';
-import RegisterForm from './components/RegisterForm';
-import SolarProviderList from './components/SolarProviderList';
-=======
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { auth } from "./Firebase";
@@ -22,7 +11,7 @@ import About from "./components/About";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 import PublicRoute from "./components/PublicRoute";
->>>>>>> 7348abf0546100e7b2ab89b2191fde14ed9e18ea
+import SolarProviderList from './components/SolarProviderList';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -42,11 +31,9 @@ function App() {
       <main className="min-h-screen bg-gradient-to-r from-green-100 to-green-300 dark:from-green-800 dark:to-green-900 text-gray-900 dark:text-white transition-all duration-300">
         <Routes>
           <Route path="/" element={<Home />} />
-<<<<<<< HEAD
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/solar" element={<SolarProviderList />} />
-=======
           <Route
             path="/login"
             element={
@@ -63,7 +50,6 @@ function App() {
               </PublicRoute>
             }
           />
->>>>>>> 7348abf0546100e7b2ab89b2191fde14ed9e18ea
           <Route path="/about" element={<About />} />
         </Routes>
       </main>
